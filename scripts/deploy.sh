664986ba2ee2782f3729b363c5716af9aee9b62b
root <root@ip-172-31-8-44.us-west-2.compute.internal>
2022-11-20 18:39:17 +0000

    added shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-
 Public_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
 DIR=/root/django-app/venv
 
-Running_PID=`ps -ef | grep "venv/bin/python" | grep -v grep | awk '{print $2}'`
-if [ -z "$Running_PID" ]
+Running_process=`systemctl is-active django.service`
+if [ $Running_process == 'active' ];
 then
-	echo "there is no process is running, hence we are moving further" >> /root/django-app/app.log
+  systemctl stop django.service
+  echo "Application has been stopped." >> /root/django-app/app.log
 else
-	kill -9 $Running_PID
+  echo "there is no process is running, hence we are moving further" >> /root/django-app/app.log
 fi
 
-echo "Application has been stopped." >> /root/django-app/app.log
-
+echo "This is: $DIR"
 if [ -d "$DIR" ];
 then
-	python3 -m venv ./venv --prompt django-app-env
-	source venv/bin/activate
-	python3 -m pip install -r /root/django-app/requirements.txt
-else
-  source venv/bin/activate
+  echo "found venv path, hence we are removing it"
+  rm -rf $DIR
   python3 -m pip install -r /root/django-app/requirements.txt
-  sed -i "/ALLOWED_HOSTS/c\ALLOWED_HOSTS = ['$Public_IP']" /root/django-app/djangoapp/settings.py
-  nohup python manage.py runserver 0.0.0.0:8000 &>/dev/null &
+else
+  python3 -m pip install -r /root/django-app/requirements.txt
 fi
 
-Status_check=`curl -o - -I  $Public_IP:8000 | head -n1 | awk '{print $2}'`
-if ["$Status_check == 200" ]
+sed -i "/ALLOWED_HOSTS/c\ALLOWED_HOSTS = ['$Public_IP']" /root/django-app/djangoapp/settings.py
+systemctl start django.service
+
+Status_check=`systemctl is-active django.service`
+if [ $Status_check == 'active' ];
 then
 echo "Application is succesfully ruuning on $Public_IP:8000" >> /root/django-app/app.log
 else
